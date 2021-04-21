0fe0aa6ece0b4df3ee5f673ea74fe49c83ea9561
Roger Huba <rogerhuba@gmail.com>
2021-04-21 17:04:03 -0500

    add code-along and demo

---
+++ class-06/swbst/app1.js
@@ -0,0 +1,4 @@
+let userName = prompt('Please Enter Your Name');
+
+document.write('<h2>' + userName + '</h2>');
+// <h2>Roger</h2>
---
+++ class-06/swbst/app2.js
@@ -0,0 +1,7 @@
+if (userName.toLowerCase() == 'roger'){
+    document.write('<h3>Welcome Darth Roger</h3>');
+}else if(userName.toLowerCase() == 'tom') {
+    document.write('<h3>Welcome Acolite Tom</h3>');
+} else {
+    document.write('<h3>Imposter</h3>');
+}
